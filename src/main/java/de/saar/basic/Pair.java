8e05d691edcf8053c8e99d880601237dd0ee023d
Unknown <koller@60261d8b-8c22-4c29-bc6b-f298eebed379>
2009-09-22 09:40:42 +0000

    Added a class for pairs.

---
+++ src/main/java/de/saar/basic/Pair.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.saar.basic;
+
+/**
+ *
+ * @author koller
+ */
+public class Pair<E,F> {
+    public E left;
+    public F right;
+
+    public Pair(E left, F right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    
+}
