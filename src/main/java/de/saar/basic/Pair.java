f7c0eca0755e8e19451ceadee7743d1448143492
Unknown <koller@60261d8b-8c22-4c29-bc6b-f298eebed379>
2012-02-05 13:31:18 +0000

    added reasonable hashCode for Pair (!!)

--- src/main/java/de/saar/basic/Pair.java
+++ src/main/java/de/saar/basic/Pair.java
@@ -2,23 +2,40 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package de.saar.basic;
 
 import java.io.Serializable;
 
 /**
- *
+ * An immutable pair. While you can modify `left' and `right' directly
+ * (an early sin in implementing this class), doing so invalidates
+ * the hashCode. So if you plan on using Pairs as keys of HashMaps,
+ * you should refrain from doing this. Also, you are encouraged to use
+ * getLeft() and getRight() instead of accessing the fields directly,
+ * as these may be made private at a future time.
+ * 
  * @author koller
  */
-public class Pair<E,F> implements Serializable {
+public class Pair<E, F> implements Serializable {
     public E left;
     public F right;
+    private int cachedHashCode;
 
     public Pair(E left, F right) {
         this.left = left;
         this.right = right;
+        cachedHashCode = 0;
     }
+
+    public E getLeft() {
+        return left;
+    }
+
+    public F getRight() {
+        return right;
+    }
+    
+    
 
     @Override
     public boolean equals(Object obj) {
@@ -28,7 +45,9 @@ public class Pair<E,F> implements Serializable {
         if (getClass() != obj.getClass()) {
             return false;
         }
+
         final Pair<E, F> other = (Pair<E, F>) obj;
+        
         if (this.left != other.left && (this.left == null || !this.left.equals(other.left))) {
             return false;
         }
@@ -40,15 +59,17 @@ public class Pair<E,F> implements Serializable {
 
     @Override
     public int hashCode() {
-        int hash = 7;
-        return hash;
+        if (cachedHashCode == 0) {
+            cachedHashCode = 7;
+            cachedHashCode = 11 * cachedHashCode + (this.left != null ? this.left.hashCode() : 0);
+            cachedHashCode = 11 * cachedHashCode + (this.right != null ? this.right.hashCode() : 0);
+        }
+        
+        return cachedHashCode;
     }
-
 
     @Override
     public String toString() {
         return left + "," + right;
     }
-
-    
 }
